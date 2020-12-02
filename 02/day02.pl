c77fb4ea2d64620b639b00f3fff93c205e9948b7
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-12-02 11:23:58 +0100

    Cleanup

--- 02/day02.pl
+++ 02/day02.pl
@@ -20,7 +20,7 @@ while (<>) {
     # Excersise 2
     $char1 = substr($4, $1 - 1, 1);
     $char2 = substr($4, $2 - 1, 1);
-    if (("$char1" eq "$3" || "$char2" eq "$3") && ("$char1" ne "$char2")) {
+    if (($char1 eq $3 || $char2 eq $3) && ($char1 ne $char2)) {
 	$c2++;
     }
 }
