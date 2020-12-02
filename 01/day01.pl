8d37ff2c949c1c3cf7cb1415c33bf407361b9a63
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-12-02 10:20:40 +0100

    Transfer day 01 to Perl

---
+++ 01/day01.pl
@@ -0,0 +1,29 @@
+# USAGE: perl day01.pl <input-file>
+
+while (<>) {
+  $_ =~ /^(\d+)$/;
+  push @t, $1;
+}
+
+# Excersise 1
+OUTER:
+for $i (0..@t-1) {
+  for $j ($i+1..@t-1) {
+    if ($t[$i] + $t[$j] == 2020) {
+      print "$t[$i] * $t[$j] = ", ($t[$i] * $t[$j]), "\n";
+      last OUTER;
+    }
+  }
+}
+
+# Excersise 2
+for $i (0..@t-1) {
+  for $j ($i+1..@t-1) {
+    for $k ($j+1..@t-1) {
+      if ($t[$i] + $t[$j] + $t[$k] == 2020) {
+	print "$t[$i] * $t[$j] * $t[$k] = ", ($t[$i] * $t[$j] * $t[$k]), "\n";
+	exit 0;
+      }
+    }
+  }
+}
