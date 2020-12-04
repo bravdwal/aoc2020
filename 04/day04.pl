273bc36d37209a7678a6a037439553bf56fe0de0
Bram Van de Walle <bram.vandewalle@ugent.be>
2020-12-04 09:35:14 +0100

    Finish day 4

--- 04/day04.pl
+++ 04/day04.pl
@@ -5,18 +5,58 @@ $/ = "";
 $c = 0;
 $cc = 0;
 
+sub valid_byr {
+    local ($byr) = @_;
+    return "$byr" =~ /^\d{4}$/ && $byr >= 1920 && $byr <= 2002 ? 1 : 0;
+}
+
+sub valid_iyr {
+    local ($iyr) = @_;
+    return $iyr =~ /^\d{4}$/ && $iyr >= 2010 && $iyr <= 2020 ? 1 : 0;
+}
+
+sub valid_eyr {
+    local ($eyr) = @_;
+    return $eyr =~ /^\d{4}$/ && $eyr >= 2020 && $eyr <= 2030 ? 1 : 0;
+}
+
+sub valid_hgt {
+    local ($hgt) = @_;
+    $hgt =~ /^(\d+)(cm|in)$/;
+
+    if ($2 eq "cm") {
+	return $1 >= 150 && $1 <= 193 ? 1 : 0;
+    } elsif ($2 eq "in") {
+	return $1 >= 59 && $1 <= 76 ? 1 : 0;
+    }
+
+    return 0;
+}
+
+sub valid_hcl {
+    local ($hcl) = @_;
+    return $hcl =~ /^#([0-9]|[a-f]){6}$/ ? 1 : 0;
+}
+
+sub valid_ecl {
+    local ($ecl) = @_;
+    return $ecl =~ /^(amb|blu|brn|gry|grn|hzl|oth)$/ ? 1 : 0;
+}
+
+sub valid_pid {
+    local ($pid) = @_;
+    return "$pid" =~ /^\d{9}$/ ? 1 : 0;
+}
+
 while (<>) {
     %h = ();
 
-    print "$_\n";
     while (/(\w+):(.*?)( |\n)/g) {
-	print "1 = $1 en 2 = $2\n";
         $h{$1} = $2;
     }
-    print "\n";
 
-    print join "\n", map {"$_ -> $h{$_}"} keys %h;
-    print "\n\n";
+    #print join "\n", map {"$_ -> $h{$_}"} keys %h;
+    #print "\n\n";
 
     # Part 1
     if (exists $h{byr} && exists $h{iyr} && exists $h{eyr} && exists $h{hgt} && exists $h{hcl} && exists $h{ecl} && exists $h{pid}) {
@@ -24,14 +64,14 @@ while (<>) {
     }
 
     # Part 2
-    if ($h{byr} =~ /\d{4}/ && $h{byr} >= 1920 && $h{byr} <= 2002 &&
-	$h{iyr} =~ /\d{4}/ && $h{iyr} >= 2010 && $h{byr} <= 2020 &&
-	$h{eyr} =~ /\d{4}/ && $h{eyr} >= 2020 && $h{byr} <= 2030 &&
-	$h{hgt} =~ /(\d+)(cm|in)/ &&
-	$h{hcl} =~ /#([0-9]{6}|[a-f]{6})/ &&
-	$h{ecl} =~ /(amb|blu|brn|gry|grn|hzl|oth)/ &&
-	$h{pid} =~ /\d{9}/) {
-	$cc++ if (($2 eq "cm" && $1 >= 150 && $1 <= 193) || ($2 eq "in" && $1 >= 59 && $1 <= 76));
+    if (valid_byr("$h{byr}") &&
+	valid_iyr("$h{iyr}") &&
+	valid_eyr("$h{eyr}") &&
+	valid_hgt("$h{hgt}") &&
+	valid_hcl("$h{hcl}") &&
+	valid_ecl("$h{ecl}") &&
+	valid_pid("$h{pid}")) {
+	$cc++;
     }
 }
 
